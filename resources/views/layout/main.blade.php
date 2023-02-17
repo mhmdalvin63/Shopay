424ea33c304250c60e8f58c910a964286c93c491
muhammadalvin92 <alvinbomber87@gmail.com>
2023-02-17 12:55:25 +0700

    First Commit

---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,344 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Kapella Bootstrap Admin Dashboard Template</title>
+    <!-- base:css -->
+    <link rel="stylesheet" href="{{ asset('template/vendors/mdi/css/materialdesignicons.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('template/vendors/base/vendor.bundle.base.css')}}">
+    <!-- endinject -->
+    <!-- plugin css for this page -->
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <link rel="stylesheet" href="{{ asset('template/css/style.css')}}">
+    <!-- endinject -->
+    <link rel="shortcut icon" href="{{ asset('template/images/favicon.png')}}" />
+  </head>
+  <body>
+    <div class="container-scroller">
+				{{-- <div class="row p-0 m-0 proBanner" id="proBanner">
+					<div class="col-md-12 p-0 m-0">
+						<div class="card-body card-body-padding d-flex align-items-center justify-content-between">
+							<div class="ps-lg-1">
+								<div class="d-flex align-items-center justify-content-between">
+									<p class="mb-0 font-weight-medium me-3 buy-now-text">Free 24/7 customer support, updates, and more with this template!</p>
+									<a href="{{ asset('template/https://www.bootstrapdash.com/product/kapella-admin-pro/?utm_source=organic&utm_medium=banner&utm_campaign=buynow_demo')}}" target="_blank" class="btn me-2 buy-now-btn border-0">Get Pro</a>
+								</div>
+							</div>
+							<div class="d-flex align-items-center justify-content-between">
+								<a href="{{ asset('template/https://www.bootstrapdash.com/product/kapella-admin-pro/')}}"><i class="mdi mdi-home me-3 text-white"></i></a>
+								<button id="bannerClose" class="btn border-0 p-0">
+									<i class="mdi mdi-close text-white me-0"></i>
+								</button>
+							</div>
+						</div>
+					</div>
+				</div> --}}
+		<!-- partial:partials/_horizontal-navbar.html -->
+    <div class="horizontal-menu">
+      <nav class="navbar top-navbar col-lg-12 col-12 p-0">
+        <div class="container-fluid">
+          <div class="navbar-menu-wrapper d-flex align-items-center justify-content-between">
+            <ul class="navbar-nav navbar-nav-left">
+              <li class="nav-item ms-0 me-5 d-lg-flex d-none">
+                <a href="#" class="nav-link horizontal-nav-left-menu"><i class="mdi mdi-format-list-bulleted"></i></a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link count-indicator dropdown-toggle d-flex align-items-center justify-content-center" id="notificationDropdown" href="#" data-bs-toggle="dropdown">
+                  <i class="mdi mdi-bell mx-0"></i>
+                  <span class="count bg-success">2</span>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="notificationDropdown">
+                  <p class="mb-0 font-weight-normal float-left dropdown-header">Notifications</p>
+                  <a class="dropdown-item preview-item">
+                    <div class="preview-thumbnail">
+                        <div class="preview-icon bg-success">
+                          <i class="mdi mdi-information mx-0"></i>
+                        </div>
+                    </div>
+                    <div class="preview-item-content">
+                        <h6 class="preview-subject font-weight-normal">Application Error</h6>
+                        <p class="font-weight-light small-text mb-0 text-muted">
+                          Just now
+                        </p>
+                    </div>
+                  </a>
+                  <a class="dropdown-item preview-item">
+                    <div class="preview-thumbnail">
+                        <div class="preview-icon bg-warning">
+                          <i class="mdi mdi-settings mx-0"></i>
+                        </div>
+                    </div>
+                    <div class="preview-item-content">
+                        <h6 class="preview-subject font-weight-normal">Settings</h6>
+                        <p class="font-weight-light small-text mb-0 text-muted">
+                          Private message
+                        </p>
+                    </div>
+                  </a>
+                  <a class="dropdown-item preview-item">
+                    <div class="preview-thumbnail">
+                        <div class="preview-icon bg-info">
+                          <i class="mdi mdi-account-box mx-0"></i>
+                        </div>
+                    </div>
+                    <div class="preview-item-content">
+                        <h6 class="preview-subject font-weight-normal">New user registration</h6>
+                        <p class="font-weight-light small-text mb-0 text-muted">
+                          2 days ago
+                        </p>
+                    </div>
+                  </a>
+                </div>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link count-indicator dropdown-toggle d-flex justify-content-center align-items-center" id="messageDropdown" href="#" data-bs-toggle="dropdown">
+                  <i class="mdi mdi-email mx-0"></i>
+                  <span class="count bg-primary">4</span>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="messageDropdown">
+                  <p class="mb-0 font-weight-normal float-left dropdown-header">Messages</p>
+                  <a class="dropdown-item preview-item">
+                    <div class="preview-thumbnail">
+                        <img src="images/faces/face4.jpg" alt="image" class="profile-pic">
+                    </div>
+                    <div class="preview-item-content flex-grow">
+                        <h6 class="preview-subject ellipsis font-weight-normal">David Grey
+                        </h6>
+                        <p class="font-weight-light small-text text-muted mb-0">
+                          The meeting is cancelled
+                        </p>
+                    </div>
+                  </a>
+                  <a class="dropdown-item preview-item">
+                    <div class="preview-thumbnail">
+                        <img src="images/faces/face2.jpg" alt="image" class="profile-pic">
+                    </div>
+                    <div class="preview-item-content flex-grow">
+                        <h6 class="preview-subject ellipsis font-weight-normal">Tim Cook
+                        </h6>
+                        <p class="font-weight-light small-text text-muted mb-0">
+                          New product launch
+                        </p>
+                    </div>
+                  </a>
+                  <a class="dropdown-item preview-item">
+                    <div class="preview-thumbnail">
+                        <img src="images/faces/face3.jpg" alt="image" class="profile-pic">
+                    </div>
+                    <div class="preview-item-content flex-grow">
+                        <h6 class="preview-subject ellipsis font-weight-normal"> Johnson
+                        </h6>
+                        <p class="font-weight-light small-text text-muted mb-0">
+                          Upcoming board meeting
+                        </p>
+                    </div>
+                  </a>
+                </div>
+              </li>
+              <li class="nav-item dropdown">
+                <a href="#" class="nav-link count-indicator "><i class="mdi mdi-message-reply-text"></i></a>
+              </li>
+              <li class="nav-item nav-search d-none d-lg-block ms-3">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text" id="search">
+                        <i class="mdi mdi-magnify"></i>
+                      </span>
+                    </div>
+                    <input type="text" class="form-control" placeholder="search" aria-label="search" aria-describedby="search">
+                </div>
+              </li>	
+            </ul>
+            <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
+                <a class="navbar-brand brand-logo" href="{{ asset('template/index.html')}}"><img src="{{asset('template/images/logo-sekolah.png')}}" style="width: 10rem; height: 10rem;" alt="logo"/></a>
+                <a class="navbar-brand brand-logo-mini" href="{{ asset('template/index.html')}}"><img src="{{asset('template/images/logo-sekolah.png')}}" style="width: 7.5rem; height: 7.5rem;" alt="logo"/></a>
+            </div>
+            <ul class="navbar-nav navbar-nav-right">
+                <li class="nav-item dropdown  d-lg-flex d-none">
+                  <button type="button" class="btn btn-inverse-primary btn-sm">Product </button>
+                </li>
+                <li class="nav-item dropdown d-lg-flex d-none">
+                  <a class="dropdown-toggle show-dropdown-arrow btn btn-inverse-primary btn-sm" id="nreportDropdown" href="#" data-bs-toggle="dropdown">
+                  Reports
+                  </a>
+                  <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="nreportDropdown">
+                      <p class="mb-0 font-weight-medium float-left dropdown-header">Reports</p>
+                      <a class="dropdown-item">
+                        <i class="mdi mdi-file-pdf text-primary"></i>
+                        Pdf
+                      </a>
+                      <a class="dropdown-item">
+                        <i class="mdi mdi-file-excel text-primary"></i>
+                        Exel
+                      </a>
+                  </div>
+                </li>
+                <li class="nav-item dropdown d-lg-flex d-none">
+                  <button type="button" class="btn btn-inverse-primary btn-sm">Settings</button>
+                </li>
+                <li class="nav-item nav-profile dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" id="profileDropdown">
+                    <span class="nav-profile-name">Nurdin</span>
+                    <span class="online-status"></span>
+                    <img src="{{asset('template/images/faces/nuyyy.jpg')}}" alt="profile"/>
+                  </a>
+                  <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
+                      <a class="dropdown-item">
+                        <i class="mdi mdi-settings text-primary"></i>
+                        Settings
+                      </a>
+                      <a class="dropdown-item">
+                        <i class="mdi mdi-logout text-primary"></i>
+                        Logout
+                      </a>
+                  </div>
+                </li>
+            </ul>
+            <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="horizontal-menu-toggle">
+              <span class="mdi mdi-menu"></span>
+            </button>
+          </div>
+        </div>
+      </nav>
+      <nav class="bottom-navbar">
+        <div class="container">
+            <ul class="nav page-navigation">
+              <li class="nav-item">
+                <a class="nav-link" href="">
+                  <i class="mdi mdi-file-document-box menu-icon"></i>
+                  <span class="menu-title">Dashboard</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                  <a href="#" class="nav-link">
+                    <i class="mdi mdi-clipboard-account menu-icon"></i>
+                    <span class="menu-title">Data Petugas</span>
+                    {{-- <i class="menu-arrow"></i> --}}
+                  </a>
+              </li>
+              <li class="nav-item">
+                  <a href="" class="nav-link">
+                    <i class="mdi mdi-account-card-details menu-icon"></i>
+                    <span class="menu-title">Data Siswa</span>
+                    <i class="menu-arrow"></i>
+                  </a>
+              </li>
+              <li class="nav-item">
+                  <a href="" class="nav-link">
+                    <i class="mdi mdi-school menu-icon"></i>
+                    <span class="menu-title">Data Kelas</span>
+                    <i class="menu-arrow"></i>
+                  </a>
+              </li>
+              <li class="nav-item">
+                  <a href="" class="nav-link">
+                    <i class="mdi mdi-coin menu-icon"></i>
+                    <span class="menu-title">Data SPP</span>
+                    <i class="menu-arrow"></i>
+                  </a>
+              </li>
+              <li class="nav-item">
+                  <a href="" class="nav-link">
+                    <i class="mdi mdi-format-align-center menu-icon"></i>
+                    <span class="menu-title">Input Transaksi</span>
+                    <i class="menu-arrow"></i>
+                  </a>
+              </li>
+              <li class="nav-item">
+                  <a href="#" class="nav-link">
+                    <i class="mdi mdi-history menu-icon"></i>
+                    <span class="menu-title">History</span>
+                    <i class="menu-arrow"></i>
+                  </a>
+              </li>
+            </ul>
+        </div>
+      </nav>
+    </div>
+    <!-- partial -->
+		<div class="container-fluid page-body-wrapper">
+			<div class="main-panel">
+				<div class="content-wrapper">
+					<div class="row">
+						<div class="col-sm-6 mb-4 mb-xl-0">
+							<div class="d-lg-flex align-items-center">
+								<div>
+									<h3 class="text-dark font-weight-bold mb-2">Hi, welcome back!</h3>
+									<h6 class="font-weight-normal mb-2">Last login was 23 hours ago. View details</h6>
+								</div>
+								<div class="ms-lg-5 d-lg-flex d-none">
+										<button type="button" class="btn bg-white btn-icon">
+											<i class="mdi mdi-view-grid text-success"></i>
+									</button>
+										<button type="button" class="btn bg-white btn-icon ms-2">
+											<i class="mdi mdi-format-list-bulleted font-weight-bold text-primary"></i>
+										</button>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-6">
+							<div class="d-flex align-items-center justify-content-md-end">
+								<div class="pe-1 mb-3 mb-xl-0">
+										<button type="button" class="btn btn-outline-inverse-info btn-icon-text">
+											Feedback
+											<i class="mdi mdi-message-outline btn-icon-append"></i>                          
+										</button>
+								</div>
+								<div class="pe-1 mb-3 mb-xl-0">
+										<button type="button" class="btn btn-outline-inverse-info btn-icon-text">
+											Help
+											<i class="mdi mdi-help-circle-outline btn-icon-append"></i>                          
+									</button>
+								</div>
+								<div class="pe-1 mb-3 mb-xl-0">
+										<button type="button" class="btn btn-outline-inverse-info btn-icon-text">
+											Print
+											<i class="mdi mdi-printer btn-icon-append"></i>                          
+										</button>
+								</div>
+							</div>
+						</div>
+					</div>
+                    @yield('content')
+				</div>
+				<!-- content-wrapper ends -->
+				<!-- partial:partials/_footer.html -->
+				<footer class="footer">
+          <div class="footer-wrap">
+            <div class="d-sm-flex justify-content-center justify-content-sm-between">
+              <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © <a href="{{ asset('template/https://www.bootstrapdash.com/')}}" target="_blank">bootstrapdash.com </a>2021</span>
+              <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Only the best <a href="{{ asset('template/https://www.bootstrapdash.com/')}}" target="_blank"> Bootstrap dashboard </a> templates</span>
+            </div>
+          </div>
+        </footer>
+				<!-- partial -->
+			</div>
+			<!-- main-panel ends -->
+		</div>
+		<!-- page-body-wrapper ends -->
+    </div>
+		<!-- container-scroller -->
+    <!-- base:js -->
+    <script src="{{asset('template/vendors/base/vendor.bundle.base.js')}} "></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page-->
+    <!-- End plugin js for this page-->
+    <!-- inject:js -->
+    <script src="{{asset('template/js/template.js')}} "></script>
+    <!-- endinject -->
+    <!-- plugin js for this page -->
+    <!-- End plugin js for this page -->
+    <script src="{{asset('template/vendors/chart.js/Chart.min.js')}} "></script>
+    <script src="{{asset('template/vendors/progressbar.js/progressbar.min.js')}} "></script>
+		<script src="{{asset('template/vendors/chartjs-plugin-datalabels/chartjs-plugin-datalabels.js')}} "></script>
+		<script src="{{asset('template/vendors/justgage/raphael-2.1.4.min.js')}} "></script>
+		<script src="{{asset('template/vendors/justgage/justgage.js')}} "></script>
+    <script src="{{asset('template/js/jquery.cookie.js')}} " type="text/javascript"></script>
+    <!-- Custom js for this page-->
+    <script src="{{asset('template/js/dashboard.js')}} "></script>
+    <!-- End custom js for this page-->
+  </body>
+</html>
